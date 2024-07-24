10dba4d6c14f1c0d88a0a19f3db855c309687288
ROKNALWADY <achrafmukhtar@gmail.com>
2024-07-24 12:11:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function openWhatsApp() {
-    var phoneNumber = "966508764175"; // Saudi Arabia WhatsApp number without the '+'
+    var phoneNumber = "966532926288"; // Saudi Arabia WhatsApp number without the '+'
     var message = "مرحبًا، أود الاستفسار عن خدمة نقل العفش.";
-    var url = "https://wa.me/" + 966508764175 + "?text=" + encodeURIComponent(message);
+    var url = "https://wa.me/" + 966532926288 + "?text=" + encodeURIComponent(message);
     window.open(url, '_blank');
 }
 function openContact() {
     // Replace with your contact page or phone number
-    window.location.href = "tel:+966508764175";
+    window.location.href = "tel:+966532926288";
 }
